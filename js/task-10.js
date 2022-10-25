4154cda5d8b5458c220cd064c8de9d400aa77dd8
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-10-25 15:29:11 +0300

    Made 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,49 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const btnCreateBox = document.querySelector("button[data-create]");
+const bntDestroyBox = document.querySelector("button[data-destroy");
+const boxesDiv = document.querySelector("#boxes");
+
+let amountBoxes = 0;
+
+input.addEventListener("input", onInputChange)
+
+function onInputChange(event) {
+  amountBoxes = event.currentTarget.value
+};
+
+function boxCreate(amount) {
+  const boxes = [];
+  let sizes = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const newDiv = document.createElement("div");
+
+    newDiv.style.width = `${sizes}px`;
+    newDiv.style.height = `${sizes}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+
+    sizes += 10;
+    boxes.push(newDiv);
+  }
+
+boxesDiv.append(...boxes);
+};
+
+
+btnCreateBox.addEventListener("click", createOnClick)
+
+function createOnClick() {
+  return boxCreate(amountBoxes);
+};
+
+bntDestroyBox.addEventListener("click", destroyOnClick)
+
+function destroyOnClick() {
+  boxesDiv.innerHTML = "";
+};
+
+console.log(boxesDiv)
